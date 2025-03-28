96e5b4d61751de1942d36dfa27802d45d472dc9f
Anu-wolf <anushri.2335@gmail.com>
2025-03-28 07:53:32 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+const darkModeToggle = document.getElementById('darkModeToggle');
+
+darkModeToggle.addEventListener('click', () => {
+    document.body.classList.toggle('dark-mode');
+
+    if (document.body.classList.contains('dark-mode')) {
+        darkModeToggle.textContent = '☀️';
+    } else {
+        darkModeToggle.textContent = '🌙';
+    }
+});
